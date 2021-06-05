bf49e9032ff1c0d8aa67c47308b3f92382417ab5
MasakoIdei <tarte.groseilles@gmail.com>
2021-06-05 09:14:42 +0000

    [finish]フォロー機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,32 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 被フォロー関係を通じて参照→followed_idをフォローしている人
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 【class_name: "Relationship"】は省略可能
+  has_many :followings, through: :relationships, source: :followed
+  # 与フォロー関係を通じて参照→follower_idをフォローしている人
+  
+  # ユーザーをフォローする
+  def follow(user_id)
+   relationships.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す/フォロワーがフォローした時に出来たidを探してきてdestroy
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+ # フォローしていればtrueを返す
+  def following?(user)
+    followings.include?(user)
+  end
+  
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
 end
